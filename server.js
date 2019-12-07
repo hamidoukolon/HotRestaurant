2675051c846f1b7c19069cbc7b1f24813c2ff0ee
Clark Gesen <cgesen@gmail.com>
2019-12-07 11:34:04 -0500

    Got JSON data displaying on reservations and waitlist paths

--- server.js
+++ server.js
@@ -5,12 +5,43 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.json(waitlist));
+// app.use(express.json(reservations));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 
-let reservations = [];
+let reservations = [
+    {
+        name: "Will",
+        phone: 60302359,
+        email: "iugnnuaoio",
+        uniqueID: 234,
+    }
+];
 
-let waitlist = [];
+let waitlist = [
+    {
+        name: "Kolon",
+        phone: 13234,
+        email: "oisngn",
+        uniqueID: 345,
+    }
+];
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "waitlist.html"));
+  });
+
+  app.get("/reservations", function(req, res) {
+    return res.json(reservations);
+  });
+
+  app.get("/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
